6c7184be5fcf273b18d9a4086f0fe1535ef457da
Jesus Lopez <jlopez92@unm.edu>
2016-02-21 14:29:26 -0700

    Created a more efficient Hall class.
    A hall is now a pair of halls, a vertical and horizontal hall that
    connect to make a path between two rooms. When a hall is created with
    the public constructor class, it reads in the starting point from room 1
    to the ending point in room 2, as well as if the initial path will be
    vertical or horizontal(arbitrary for now). The class then determines a
    point that is a straight line away from both the starting point and the
    ending point, and creates a horizontal and vertical path connecting both
    rooms.

--- TestGit/src/RoomGenerator/Hall.java
+++ TestGit/src/RoomGenerator/Hall.java
@@ -1,13 +1,13 @@
 package RoomGenerator;
 
 public class Hall {
-//	private static final int HALL_WIDTH = 2;
+	// private static final int HALL_WIDTH = 2;
+	private Hall connectingHall;
 	private int startX;
 	private int startY;
 	private int endX;
 	private int endY;
-	private int width;
-	private int height;
+
 	private boolean isVertical;
 
 	public Hall(int x1, int y1, int targetX, int targetY, boolean isVertical) {
@@ -15,49 +15,66 @@ public class Hall {
 		startY = y1;
 		this.isVertical = isVertical;
 		makeHall(targetX, targetY);
+		makeConnectingHall(targetX, targetY);
+	}
+
+	private Hall(int x1, int y1, int endX, int endY, boolean isVertical, Hall neighbor) {
+		startX = x1;
+		startY = y1;
+		this.endX = endX;
+		this.endY = endY;
+		this.isVertical = isVertical;
+		connectingHall = neighbor;
+	}
+
+	/*
+	 * Creates the neighboring hall that is the opposite type of the first one
+	 * made. If first hall is vertical, the neighbor is horizontal, and vice
+	 * versa.
+	 */
+	private void makeConnectingHall(int targetX, int targetY) {
+		connectingHall = new Hall(this.endX, this.endY, targetX, targetY, !isVertical, this);
 	}
 
 	private void makeHall(int targetX, int targetY) {
 		if (isVertical) {
+			endX = startX;
 			endY = targetY;
-			endX = getStartX();
-			width = 1;
-			height = startY - endY;
 		} else {
 			endX = targetX;
-			endY = getStartY();
-			width = getStartX() - endX;
-			height = 1;
+			endY = startY;
 		}
 	}
 
-	public void printCoordinates() {
-		System.out.println(
-				"startX:  " + startX + "  startY:  " + startY + "  endX: " + endX + "   endY: " + endY);
-		System.out.println("WIDTH:  " + (startX - endX) + "    HEIGHT : " + (startY - endY));
-	}
+	// public void printCoordinates() {
+	// System.out.println(
+	// "startX: " + startX + " startY: " + startY + " endX: " + endX + " endY: "
+	// + endY);
+	// System.out.println("WIDTH: " + (startX - endX) + " HEIGHT : " + (startY -
+	// endY));
+	// }
 
 	public int getStartY() {
 		return startY;
 	}
 
-	public int getHeight() {
-		return height;
-	}
-
 	public int getStartX() {
 		return startX;
 	}
-	
-	public int getEndX(){
-		return endX;
-	}
-	
-	public int getEndY(){
-		return endY;
+
+	public boolean isVertical() {
+		return isVertical;
 	}
 
-	public int getWidth() {
-		return width;
+	public int getEndX() {
+		return endX;
+	}
+
+	public int getEndY() {
+		return endY;
+	}
+	
+	public Hall getNeighbor(){
+		return connectingHall;
 	}
 }
