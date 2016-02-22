95343bdac7cc9191e4a480136113e87ab0b783a1
Jesus Lopez <jlopez92@unm.edu>
2016-02-21 23:50:52 -0700

    Creates RandomWalk class that extends Zombie.
    Abstract method left unimplemented.

---
+++ TestGit/src/CPU/RandomWalk.java
@@ -0,0 +1,18 @@
+package CPU;
+
+public class RandomWalk extends Zombie
+{
+
+  public RandomWalk(int x, int y)
+  {
+    super(x, y);
+  }
+
+  @Override
+  void determineNextMove()
+  {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
