26c162b5ecfc3dd2fb1cc5628616e9ac67ba1985
Jesus Lopez <jlopez92@unm.edu>
2016-02-21 23:44:50 -0700

    Introduces the CPU package and the Zombie class.
    Right now all a Zombie is just something with a starting point.

---
+++ TestGit/src/CPU/Zombie.java
@@ -0,0 +1,13 @@
+package CPU;
+
+public class Zombie
+{
+  private int startPointX;
+  private int startPointY;
+
+  public Zombie(int x, int y)
+  {
+    startPointX = x;
+    startPointY = y;
+  }
+}
