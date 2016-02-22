b1498f77ceb702a12de7f97e6d0ee70c254729a9
Jesus Lopez <jlopez92@unm.edu>
2016-02-21 23:46:38 -0700

    Formats the code to the correct CS351 coding standard.

--- TestGit/src/RoomGenerator/RoomGenerator.java
+++ TestGit/src/RoomGenerator/RoomGenerator.java
@@ -18,220 +18,267 @@ package RoomGenerator;
 
 import java.util.Random;
 
-public class RoomGenerator {
-	private final int MIN_ROOM_WIDTH = 4; // arbitrary,
-	private final int MAX_ROOM_WIDTH = 6; // arbitrary, will change once we have
-											// a better understanding of how big
-											// the map should be
-	private final int MIN_ROOM_HEIGHT = 3;// arbitrary,
+public class RoomGenerator
+{
+  private final int MIN_ROOM_WIDTH = 4; // arbitrary,
+  private final int MAX_ROOM_WIDTH = 6; // arbitrary, will change once we have a
+                                        // better understanding of how big the
+                                        // map should be
+  private final int MIN_ROOM_HEIGHT = 3;// arbitrary,
 
-	private Room[] rooms; // array of all the rooms
-	private Hall[] halls; // array of all the halls
-	private char[][] house; // The map
+  private Room[] rooms; // array of all the rooms
+  private Hall[] halls; // array of all the halls
+  private char[][] house; // The map
 
-	private int mapWidth;
-	private int mapHeight;
+  private int mapWidth;
+  private int mapHeight;
 
-	Random rand;
+  Random rand;
 
-	public RoomGenerator(int w, int h) {
-		mapWidth = w;
-		mapHeight = h;
+  public RoomGenerator(int w, int h)
+  {
+    mapWidth = w;
+    mapHeight = h;
 
-		house = new char[h][w];
+    house = new char[h][w];
 
-		rooms = new Room[5]; // currently set up to have only 5 rooms
-		halls = new Hall[40]; // 40 total halls: 20 vertical halls and 20
-								// horizontal halls means 20 logical halls.
-								// Never reaches this many with only 5 rooms.
-		rand = new Random();
-		cleanMap();
-		makeRooms();
-		printRooms();
-		makeHalls();
-		// printRooms();
-		printHalls();
-		System.out.println();
-		System.out.println();
-		printMap();
-	}
+    rooms = new Room[5]; // currently set up to have only 5 rooms
+    halls = new Hall[40]; // 40 total halls: 20 vertical halls and 20
+    // horizontal halls means 20 logical halls.
+    // Never reaches this many with only 5 rooms.
+    rand = new Random();
+    cleanMap();
+    makeRooms();
+    printRooms();
+    makeHalls();
+    // printRooms();
+    printHalls();
+    System.out.println();
+    System.out.println();
+    printMap();
+  }
 
-	private void cleanMap() {
-		for (int i = 0; i < mapHeight; i++) {
-			for (int j = 0; j < mapWidth; j++) {
-				house[i][j] = 'X';
-			}
-		}
-	}
+  private void cleanMap()
+  {
+    for (int i = 0; i < mapHeight; i++)
+    {
+      for (int j = 0; j < mapWidth; j++)
+      {
+        house[i][j] = 'X';
+      }
+    }
+  }
 
-	private void printMap() {
-		for (int i = 0; i < mapHeight; i++) {
-			for (int j = 0; j < mapWidth; j++) {
-				System.out.print(house[i][j]);
-			}
-			System.out.println();
-		}
-	}
+  private void printMap()
+  {
+    for (int i = 0; i < mapHeight; i++)
+    {
+      for (int j = 0; j < mapWidth; j++)
+      {
+        System.out.print(house[i][j]);
+      }
+      System.out.println();
+    }
+  }
 
-	private void makeHalls() {
-		Room targetRoom;
-		boolean found = false;
+  private void makeHalls()
+  {
+    Room targetRoom;
+    boolean found = false;
 
-		int startX = 0;
-		int startY = 0;
-		int targetX = 0;
-		int targetY = 0;
-		int hallCounter = 0; // keeps track of how many halls we have.
+    int startX = 0;
+    int startY = 0;
+    int targetX = 0;
+    int targetY = 0;
+    int hallCounter = 0; // keeps track of how many halls we have.
 
-		for (Room currentRoom : rooms) {
-			while (!found) {
-				found = true;
-				targetRoom = rooms[rand.nextInt(5)];
-				if (!(currentRoom.equals(targetRoom))) {
+    for (Room currentRoom : rooms)
+    {
+      while (!found)
+      {
+        found = true;
+        targetRoom = rooms[rand.nextInt(5)];
+        if (!(currentRoom.equals(targetRoom)))
+        {
 
-					// if its the first room in the array
-					if (currentRoom.equals(rooms[0])) {
-						currentRoom.setIsConnected(true);
-						targetRoom.setIsConnected(true);
-					}
+          // if its the first room in the array
+          if (currentRoom.equals(rooms[0]))
+          {
+            currentRoom.setIsConnected(true);
+            targetRoom.setIsConnected(true);
+          }
 
-					// if the target room is connected
-					// rooms only make hallways to connected rooms to prevent
-					// unreachable rooms
-					if (targetRoom.isConnected()) {
-						// current room found a valid path and is now connected
-						// to the whole map
-						currentRoom.setIsConnected(true);
+          // if the target room is connected
+          // rooms only make hallways to connected rooms to prevent
+          // unreachable rooms
+          if (targetRoom.isConnected())
+          {
+            // current room found a valid path and is now connected
+            // to the whole map
+            currentRoom.setIsConnected(true);
 
-						startX = currentRoom.getCenterX();
-						startY = currentRoom.getCenterY();
-						targetX = targetRoom.getCenterX();
-						targetY = targetRoom.getCenterY();
+            startX = currentRoom.getCenterX();
+            startY = currentRoom.getCenterY();
+            targetX = targetRoom.getCenterX();
+            targetY = targetRoom.getCenterY();
 
-						// make horizontal and vertical hallway
-						// from startXY and endXY
-						// add to halls[hallCounter]
+            // make horizontal and vertical hallway
+            // from startXY and endXY
+            // add to halls[hallCounter]
 
-						// each hall from one room to the other consists of two
-						// halls,
-						// a vertical one and a horizontal one.
+            // each hall from one room to the other consists of two
+            // halls,
+            // a vertical one and a horizontal one.
 
-						// this makes a vertical hall
-						halls[hallCounter] = new Hall(startX, startY, targetX, targetY, true);
-						addHallToMap(halls[hallCounter]);
-						hallCounter++;
+            // this makes a vertical hall
+            halls[hallCounter] = new Hall(startX, startY, targetX, targetY, true);
+            addHallToMap(halls[hallCounter]);
+            hallCounter++;
 
-						// adds the neighboring horizontal wall
-						halls[hallCounter] = halls[hallCounter - 1].getNeighbor();
-						addHallToMap(halls[hallCounter]);
-						hallCounter++;
-					} else {
-						found = false;
-					}
+            // adds the neighboring horizontal wall
+            halls[hallCounter] = halls[hallCounter - 1].getNeighbor();
+            addHallToMap(halls[hallCounter]);
+            hallCounter++;
+          }
+          else
+          {
+            found = false;
+          }
 
-				} else {
-					found = false;
-				}
-			}
-			found = false;
-		}
-	}
+        }
+        else
+        {
+          found = false;
+        }
+      }
+      found = false;
+    }
+  }
 
-	/*
-	 * If the hall is vertical, traverse only through the y positions to add
-	 * path. Else, traverse only through the x positions for horizontal path.
-	 */
+  /*
+   * If the hall is vertical, traverse only through the y positions to add path.
+   * Else, traverse only through the x positions for horizontal path.
+   */
 
-	private void addHallToMap(Hall hall) {
-		if (hall.isVertical()) {
-			if (hall.getEndY() > hall.getStartY()) {
-				for (int i = hall.getStartY(); i <= hall.getEndY(); i++) {
-					house[i][hall.getStartX()] = 'O';
-				}
-			} else {
-				for (int i = hall.getEndY(); i <= hall.getStartY(); i++) {
-					house[i][hall.getStartX()] = 'O';
-				}
-			}
-		} else {
-			if (hall.getEndX() > hall.getStartY()) {
-				for (int i = hall.getStartX(); i <= hall.getEndX(); i++) {
-					house[hall.getStartY()][i] = 'O';
-				}
-			} else {
-				for (int i = hall.getEndX(); i <= hall.getStartX(); i++) {
-					house[hall.getStartY()][i] = 'O';
-				}
-			}
-		}
-	}
+  private void addHallToMap(Hall hall)
+  {
+    if (hall.isVertical())
+    {
+      if (hall.getEndY() > hall.getStartY())
+      {
+        for (int i = hall.getStartY(); i <= hall.getEndY(); i++)
+        {
+          house[i][hall.getStartX()] = 'O';
+        }
+      }
+      else
+      {
+        for (int i = hall.getEndY(); i <= hall.getStartY(); i++)
+        {
+          house[i][hall.getStartX()] = 'O';
+        }
+      }
+    }
+    else
+    {
+      if (hall.getEndX() > hall.getStartY())
+      {
+        for (int i = hall.getStartX(); i <= hall.getEndX(); i++)
+        {
+          house[hall.getStartY()][i] = 'O';
+        }
+      }
+      else
+      {
+        for (int i = hall.getEndX(); i <= hall.getStartX(); i++)
+        {
+          house[hall.getStartY()][i] = 'O';
+        }
+      }
+    }
+  }
 
-	private void makeRooms() {
-		int startX = 0;
-		int startY = 0;
-		int width = 0;
-		int height = 0;
-		boolean hasFoundLegalSpot = false;
-		Room r;
+  private void makeRooms()
+  {
+    int startX = 0;
+    int startY = 0;
+    int width = 0;
+    int height = 0;
+    boolean hasFoundLegalSpot = false;
+    Room r;
 
-		for (int i = 0; i < rooms.length; i++) {
-			while (!hasFoundLegalSpot) {
-				hasFoundLegalSpot = true;
-				startX = rand.nextInt(13);
-				startY = rand.nextInt(16);
-				width = rand.nextInt(MAX_ROOM_WIDTH - MIN_ROOM_WIDTH + 1) + MIN_ROOM_WIDTH;
-				height = rand.nextInt(width - MIN_ROOM_HEIGHT) + MIN_ROOM_HEIGHT;
+    for (int i = 0; i < rooms.length; i++)
+    {
+      while (!hasFoundLegalSpot)
+      {
+        hasFoundLegalSpot = true;
+        startX = rand.nextInt(13);
+        startY = rand.nextInt(16);
+        width = rand.nextInt(MAX_ROOM_WIDTH - MIN_ROOM_WIDTH + 1) + MIN_ROOM_WIDTH;
+        height = rand.nextInt(width - MIN_ROOM_HEIGHT) + MIN_ROOM_HEIGHT;
 
-				if (i != 0) {
-					r = new Room(startX, startY, width, height);
-					for (int j = 0; j < i; j++) {
-						if (intersection(r, rooms[j])) {
-							hasFoundLegalSpot = false;
-						}
-					}
-				}
-			}
+        if (i != 0)
+        {
+          r = new Room(startX, startY, width, height);
+          for (int j = 0; j < i; j++)
+          {
+            if (intersection(r, rooms[j]))
+            {
+              hasFoundLegalSpot = false;
+            }
+          }
+        }
+      }
 
-			hasFoundLegalSpot = false;
-			rooms[i] = new Room(startX, startY, width, height);
-			addRoomToMap(rooms[i]);
-		}
-	}
+      hasFoundLegalSpot = false;
+      rooms[i] = new Room(startX, startY, width, height);
+      addRoomToMap(rooms[i]);
+    }
+  }
 
-	private void addRoomToMap(Room room) {
-		for (int i = room.getStartY(); i < (room.getStartY() + room.getHeight()); i++) {
-			for (int j = room.getStartX(); j < (room.getStartX() + room.getWidth()); j++) {
-				house[i][j] = 'O';
-			}
-		}
-	}
+  private void addRoomToMap(Room room)
+  {
+    for (int i = room.getStartY(); i < (room.getStartY() + room.getHeight()); i++)
+    {
+      for (int j = room.getStartX(); j < (room.getStartX() + room.getWidth()); j++)
+      {
+        house[i][j] = 'O';
+      }
+    }
+  }
 
-	private void printRooms() {
-		for (int i = 0; i < rooms.length; i++) {
-			rooms[i].printCoordinates();
-		}
-	}
+  private void printRooms()
+  {
+    for (int i = 0; i < rooms.length; i++)
+    {
+      rooms[i].printCoordinates();
+    }
+  }
 
-	private void printHalls() {
-		for (int i = 0; i < 10; i++) {
-			halls[i].printCoordinates();
-		}
-	}
+  private void printHalls()
+  {
+    for (int i = 0; i < 10; i++)
+    {
+      halls[i].printCoordinates();
+    }
+  }
 
-	public char[][] getMap() {
-		return house;
-	}
+  public char[][] getMap()
+  {
+    return house;
+  }
 
-	private boolean intersection(Room r1, Room r2) {
-		if (r1.getStartX() > r2.getStartX() + r2.getWidth() || r1.getStartX() + r1.getWidth() < r2.getStartX()
-				|| r1.getStartY() > r2.getStartY() + r2.getHeight()
-				|| r1.getStartY() + r1.getHeight() < r2.getStartY()) {
-			return false;
-		}
-		return true;
-	}
+  private boolean intersection(Room r1, Room r2)
+  {
+    if (r1.getStartX() > r2.getStartX() + r2.getWidth() || r1.getStartX() + r1.getWidth() < r2.getStartX() || r1.getStartY() > r2.getStartY() + r2.getHeight() || r1.getStartY() + r1.getHeight() < r2.getStartY())
+    {
+      return false;
+    }
+    return true;
+  }
 
-	public static void main(String[] args) {
-		RoomGenerator rg = new RoomGenerator(20, 20);
-	}
+  public static void main(String[] args)
+  {
+    RoomGenerator rg = new RoomGenerator(20, 20);
+  }
 }
