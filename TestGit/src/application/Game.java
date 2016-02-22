5bb6093a5f873b15bab7948910210b6ad098399f
Jesus Lopez <jlopez92@unm.edu>
2016-02-22 15:29:00 -0700

    Added handleSecondaryButtonDown method for unused action.
    Allows the camera to look up and down. The light doesn't follow.

--- TestGit/src/application/Game.java
+++ TestGit/src/application/Game.java
@@ -44,280 +44,323 @@ import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Scale;
 import javafx.stage.Stage;
 
-public class Game extends Application {
+public class Game extends Application
+{
 
-	private static final double TILE_SIZE = 10; // number of subdivisions in
-												// tile
-	private static final double WALL_HEIGHT = 16;
-	private static final double CAMERA_INITIAL_DISTANCE = 0;
-	private static final double CAMERA_INITIAL_X_ANGLE = 0;
-	private static final double CAMERA_INITIAL_Y_ANGLE = 0;
-	private static final double CAMERA_NEAR_CLIP = 0.1;
-	private static final double CAMERA_FAR_CLIP = 10000.0;
-	private static final double CONTROL_MULTIPLIER = 0.1;
-	private static final double SHIFT_MULTIPLIER = 10.0;
-	private static final double MOUSE_SPEED = 0.1;
-	private static final double ROTATION_SPEED = 2.0;
-	private static final double TRACK_SPEED = 0.3;
+  private static final double TILE_SIZE = 10; // number of subdivisions in
+  // tile
+  private static final double WALL_HEIGHT = 16;
+  private static final double CAMERA_INITIAL_DISTANCE = 0;
+  private static final double CAMERA_INITIAL_X_ANGLE = 0;
+  private static final double CAMERA_INITIAL_Y_ANGLE = 0;
+  private static final double CAMERA_NEAR_CLIP = 0.1;
+  private static final double CAMERA_FAR_CLIP = 10000.0;
+  private static final double CONTROL_MULTIPLIER = 0.1;
+  private static final double SHIFT_MULTIPLIER = 10.0;
+  private static final double MOUSE_SPEED = 0.1;
+  private static final double ROTATION_SPEED = 2.0;
+  private static final double TRACK_SPEED = 0.3;
 
-	private double scaleVal = 1;
+  private double scaleVal = 1;
 
-	private final Group root = new Group();
-	private final Xform world = new Xform();
-	private final PerspectiveCamera camera = new PerspectiveCamera(true);
-	private final PointLight light = new PointLight(Color.WHITE);
-	private final Xform cameraXform = new Xform();
-	private final Xform cameraXform2 = new Xform();
-	private final Xform cameraXform3 = new Xform();
+  private final Group root = new Group();
+  private final Xform world = new Xform();
+  private final PerspectiveCamera camera = new PerspectiveCamera(true);
+  private final PointLight light = new PointLight(Color.WHITE);
+  private final Xform cameraXform = new Xform();
+  private final Xform cameraXform2 = new Xform();
+  private final Xform cameraXform3 = new Xform();
 
-	private final Xform lightXform = new Xform();
-	private final Xform lightXform2 = new Xform();
-	private final Xform lightXform3 = new Xform();
-	private final Group lightGroup = new Group();
+  private final Xform lightXform = new Xform();
+  private final Xform lightXform2 = new Xform();
+  private final Xform lightXform3 = new Xform();
+  private final Group lightGroup = new Group();
 
-	private Xform mapXform = new Xform();
+  private Xform mapXform = new Xform();
 
-	private char[][] tiles;
-	private int mapH = 36;
-	private int mapW = 36;
+  private char[][] tiles;
+  private int mapH = 36;
+  private int mapW = 36;
 
-	private double windowX = 1024;
-	private double windowY = 768;
-	private boolean front = false;
-	private boolean back = false;
-	private boolean left = false;
-	private boolean right = false;
-	private double speed = .5;
+  private double windowX = 1024;
+  private double windowY = 768;
+  private boolean front = false;
+  private boolean back = false;
+  private boolean left = false;
+  private boolean right = false;
+  private double speed = .5;
 
-	private double mousePosX;
-	private double mousePosY;
-	private double mouseOldX;
-	private double mouseOldY;
-	private double mouseDeltaX;
-	private double mouseDeltaY;
+  private double mousePosX;
+  private double mousePosY;
+  private double mouseOldX;
+  private double mouseOldY;
+  private double mouseDeltaX;
+  private double mouseDeltaY;
 
-	private void buildCamera() {
-		root.getChildren().add(cameraXform);
-		cameraXform.getChildren().add(cameraXform2);
-		cameraXform2.getChildren().add(cameraXform3);
-		cameraXform3.getChildren().add(camera);
-		cameraXform3.setRotateZ(180.0);// sets y up
+  private void buildCamera()
+  {
+    root.getChildren().add(cameraXform);
+    cameraXform.getChildren().add(cameraXform2);
+    cameraXform2.getChildren().add(cameraXform3);
+    cameraXform3.getChildren().add(camera);
+    cameraXform3.setRotateZ(180.0);// sets y up
 
-		camera.setNearClip(CAMERA_NEAR_CLIP);
-		camera.setFarClip(CAMERA_FAR_CLIP);
-		camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
-		cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
-		cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
-		cameraXform.setTranslateY(WALL_HEIGHT / 2);
-	}
+    camera.setNearClip(CAMERA_NEAR_CLIP);
+    camera.setFarClip(CAMERA_FAR_CLIP);
+    camera.setTranslateZ(CAMERA_INITIAL_DISTANCE);
+    cameraXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
+    cameraXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
+    cameraXform.setTranslateY(WALL_HEIGHT / 2);
+  }
 
-	private void buildLight() {
+  private void buildLight()
+  {
 
-		light.setTranslateZ(CAMERA_INITIAL_DISTANCE);
-		lightXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
-		lightXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
+    light.setTranslateZ(CAMERA_INITIAL_DISTANCE);
+    lightXform.ry.setAngle(CAMERA_INITIAL_Y_ANGLE);
+    lightXform.rx.setAngle(CAMERA_INITIAL_X_ANGLE);
 
-		light.setTranslateX(CAMERA_INITIAL_X_ANGLE);
-		light.setTranslateY(CAMERA_INITIAL_Y_ANGLE);
-		cameraXform.getChildren().add(lightXform);// add light to camera so they
-													// move together
-		lightGroup.getChildren().add(light);
+    light.setTranslateX(CAMERA_INITIAL_X_ANGLE);
+    light.setTranslateY(CAMERA_INITIAL_Y_ANGLE);
+    cameraXform.getChildren().add(lightXform);// add light to camera so they
+    // move together
+    lightGroup.getChildren().add(light);
 
-	}
+  }
 
-	private void drawMap() {
-		// Material for floors and ceilings//
-		PhongMaterial pathable = new PhongMaterial();
-		pathable.setDiffuseColor(Color.DARKGREEN);
-		pathable.setSpecularColor(Color.ORANGE);
+  private void drawMap()
+  {
+    // Material for floors and ceilings//
+    PhongMaterial pathable = new PhongMaterial();
+    pathable.setDiffuseColor(Color.DARKGREEN);
+    pathable.setSpecularColor(Color.ORANGE);
 
-		// material for walls (this and one above may need to be the same, check
-		// ruberic//
-		PhongMaterial notPathable = new PhongMaterial();
-		notPathable.setDiffuseColor(Color.LIGHTGREEN);
-		notPathable.setSpecularColor(Color.ORANGE);
+    // material for walls (this and one above may need to be the same, check
+    // ruberic//
+    PhongMaterial notPathable = new PhongMaterial();
+    notPathable.setDiffuseColor(Color.LIGHTGREEN);
+    notPathable.setSpecularColor(Color.ORANGE);
 
-		Xform tileXform = new Xform();
-		mapXform.getChildren().add(tileXform);
+    Xform tileXform = new Xform();
+    mapXform.getChildren().add(tileXform);
 
-		// loops through a 2d array, generates rectangles of wall and floor
-		// tiles//
-		for (int i = 0; i < mapH; i++) {
-			for (int j = 0; j < mapW; j++) {
+    // loops through a 2d array, generates rectangles of wall and floor
+    // tiles//
+    for (int i = 0; i < mapH; i++)
+    {
+      for (int j = 0; j < mapW; j++)
+      {
 
-				Box tile = new Box(TILE_SIZE, 1, TILE_SIZE);
-				tile.setDrawMode(DrawMode.FILL);
-				tile.setTranslateX(i * TILE_SIZE);
-				tile.setTranslateZ(j * TILE_SIZE);
-				if (tiles[i][j] == 'O')// make a floot tile//
-				{
-					tile.setTranslateY(0.5);
-					tile.setMaterial(pathable);
-				} else// make a wall tile//
-				{
-					tile.setScaleY(WALL_HEIGHT);
-					tile.setTranslateY(WALL_HEIGHT / 2);
-					tile.setMaterial(notPathable);
-				}
-				tileXform.getChildren().add(tile);
-			}
-		}
+        Box tile = new Box(TILE_SIZE, 1, TILE_SIZE);
+        tile.setDrawMode(DrawMode.FILL);
+        tile.setTranslateX(i * TILE_SIZE);
+        tile.setTranslateZ(j * TILE_SIZE);
+        if (tiles[i][j] == 'O')// make a floot tile//
+        {
+          tile.setTranslateY(0.5);
+          tile.setMaterial(pathable);
+        }
+        else// make a wall tile//
+        {
+          tile.setScaleY(WALL_HEIGHT);
+          tile.setTranslateY(WALL_HEIGHT / 2);
+          tile.setMaterial(notPathable);
+        }
+        tileXform.getChildren().add(tile);
+      }
+    }
 
-		world.getChildren().add(mapXform);
+    world.getChildren().add(mapXform);
 
-	}
+  }
 
-	private void handleMouse(Scene scene, final Node root) {
-		scene.setOnMousePressed(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent me) {
-				mousePosX = me.getSceneX();
-				mousePosY = me.getSceneY();
-				mouseOldX = me.getSceneX();
-				mouseOldY = me.getSceneY();
-			}
-		});
-		scene.setOnMouseDragged(new EventHandler<MouseEvent>() {
-			@Override
-			public void handle(MouseEvent me) {
-				mouseOldX = mousePosX;
-				mouseOldY = mousePosY;
-				mousePosX = me.getSceneX();
-				mousePosY = me.getSceneY();
-				mouseDeltaX = (mousePosX - mouseOldX);
-				mouseDeltaY = (mousePosY - mouseOldY);
+  private void handleMouse(Scene scene, final Node root)
+  {
+    scene.setOnMousePressed(new EventHandler<MouseEvent>()
+    {
+      @Override
+      public void handle(MouseEvent me)
+      {
+        mousePosX = me.getSceneX();
+        mousePosY = me.getSceneY();
+        mouseOldX = me.getSceneX();
+        mouseOldY = me.getSceneY();
+      }
+    });
+    scene.setOnMouseDragged(new EventHandler<MouseEvent>()
+    {
+      @Override
+      public void handle(MouseEvent me)
+      {
+        mouseOldX = mousePosX;
+        mouseOldY = mousePosY;
+        mousePosX = me.getSceneX();
+        mousePosY = me.getSceneY();
+        mouseDeltaX = (mousePosX - mouseOldX);
+        mouseDeltaY = (mousePosY - mouseOldY);
 
-				double modifier = 1.0;
+        double modifier = 1.0;
 
-				if (me.isControlDown()) {
-					modifier = CONTROL_MULTIPLIER;
-				}
-				if (me.isShiftDown()) {
-					modifier = SHIFT_MULTIPLIER;
-				}
-				if (me.isPrimaryButtonDown()) {
+        if (me.isControlDown())
+        {
+          modifier = CONTROL_MULTIPLIER;
+        }
+        if (me.isShiftDown())
+        {
+          modifier = SHIFT_MULTIPLIER;
+        }
+        if (me.isPrimaryButtonDown())
+        {
 
-					lightXform.ry.setPivotX(lightXform2.t.getTx());
-					lightXform.ry.setPivotZ(lightXform2.t.getTz());
-					lightXform.ry
-							.setAngle(lightXform.ry.getAngle() - mouseDeltaX * MOUSE_SPEED * modifier * ROTATION_SPEED);
+          lightXform.ry.setPivotX(lightXform2.t.getTx());
+          lightXform.ry.setPivotZ(lightXform2.t.getTz());
+          lightXform.ry.setAngle(lightXform.ry.getAngle() - mouseDeltaX * MOUSE_SPEED * modifier * ROTATION_SPEED);
 
-					cameraXform.ry.setPivotX(cameraXform2.t.getTx());
-					cameraXform.ry.setPivotZ(cameraXform2.t.getTz());
-					cameraXform.ry.setAngle(
-							cameraXform.ry.getAngle() - mouseDeltaX * MOUSE_SPEED * modifier * ROTATION_SPEED);
-				} else if (me.isSecondaryButtonDown()) {
-					System.out.println("No effect yet");// replace with effect
+          cameraXform.ry.setPivotX(cameraXform2.t.getTx());
+          cameraXform.ry.setPivotZ(cameraXform2.t.getTz());
+          cameraXform.ry.setAngle(cameraXform.ry.getAngle() - mouseDeltaX * MOUSE_SPEED * modifier * ROTATION_SPEED);
+        }
+        else if (me.isSecondaryButtonDown())
+        {
+          // System.out.println("No effect yet");// replace with effect
+          handleSecondaryButtonDown(modifier);
 
-				} else if (me.isMiddleButtonDown())// raise and lower camera
-				{
-					lightXform2.t.setY(lightXform2.t.getY() + mouseDeltaY * MOUSE_SPEED * modifier * TRACK_SPEED);
-					cameraXform2.t.setY(cameraXform2.t.getY() + mouseDeltaY * MOUSE_SPEED * modifier * TRACK_SPEED);
+        }
+        else if (me.isMiddleButtonDown())// raise and lower camera
+        {
+          lightXform2.t.setY(lightXform2.t.getY() + mouseDeltaY * MOUSE_SPEED * modifier * TRACK_SPEED);
+          cameraXform2.t.setY(cameraXform2.t.getY() + mouseDeltaY * MOUSE_SPEED * modifier * TRACK_SPEED);
 
-				}
-			}
-		});
-	}
+        }
+      }
+    });
+  }
 
-	private void handleKeyboard(Scene scene, final Node root) {
-		scene.setOnKeyPressed(new EventHandler<KeyEvent>() {
-			@Override
-			public void handle(KeyEvent event) {
-				String s = event.getText();
-				if (s.equals("w"))
-					front = true;
-				if (s.equals("s"))
-					back = true;
-				if (s.equals("a"))
-					left = true;
-				if (s.equals("d"))
-					right = true;
+  /*
+   * Allows you to look up and down. The light doesn't follow but since being
+   * able to look up and down isn't required it doesn't matter. Nice for
+   * debugging
+   */
+  private void handleSecondaryButtonDown(double modifier)
+  {
+    cameraXform.rx.setPivotY(cameraXform2.t.getTz());
+    cameraXform.rx.setPivotZ(cameraXform2.t.getTz());
+    cameraXform.rx.setAngle(cameraXform.rx.getAngle() - mouseDeltaY * MOUSE_SPEED * modifier * ROTATION_SPEED);
+  }
 
-			}
-		});
-		scene.setOnKeyReleased(new EventHandler<KeyEvent>() {
-			@Override
-			public void handle(KeyEvent event) {
-				String s = event.getText();
-				if (s.equals("w"))
-					front = false;
-				if (s.equals("s"))
-					back = false;
-				if (s.equals("a"))
-					left = false;
-				if (s.equals("d"))
-					right = false;
+  private void handleKeyboard(Scene scene, final Node root)
+  {
+    scene.setOnKeyPressed(new EventHandler<KeyEvent>()
+    {
+      @Override
+      public void handle(KeyEvent event)
+      {
+        String s = event.getText();
+        if (s.equals("w"))
+          front = true;
+        if (s.equals("s"))
+          back = true;
+        if (s.equals("a"))
+          left = true;
+        if (s.equals("d"))
+          right = true;
 
-			}
-		});
-	}
+      }
+    });
+    scene.setOnKeyReleased(new EventHandler<KeyEvent>()
+    {
+      @Override
+      public void handle(KeyEvent event)
+      {
+        String s = event.getText();
+        if (s.equals("w"))
+          front = false;
+        if (s.equals("s"))
+          back = false;
+        if (s.equals("a"))
+          left = false;
+        if (s.equals("d"))
+          right = false;
 
-	@Override
-	public void start(Stage primaryStage) throws Exception {
-		root.getChildren().add(world);
-		world.getTransforms().add(new Scale(scaleVal, scaleVal, scaleVal));
+      }
+    });
+  }
 
-		RoomGenerator newRoom = new RoomGenerator(mapW, mapH);
-		tiles = newRoom.getMap();
+  @Override
+  public void start(Stage primaryStage) throws Exception
+  {
+    root.getChildren().add(world);
+    world.getTransforms().add(new Scale(scaleVal, scaleVal, scaleVal));
 
-		buildCamera();
-		buildLight();
-		drawMap();
+    RoomGenerator newRoom = new RoomGenerator(mapW, mapH);
+    tiles = newRoom.getMap();
 
-		Scene scene = new Scene(root, windowX, windowY, true);
-		Stop[] stops = new Stop[] { new Stop(0, Color.RED), new Stop(1, Color.ORANGE) };
-		LinearGradient lg = new LinearGradient(0, 1, 0, 0, true, CycleMethod.NO_CYCLE, stops);
-		scene.setFill(lg);
-		scene.setCamera(camera);
+    buildCamera();
+    buildLight();
+    drawMap();
 
-		handleMouse(scene, world);
-		handleKeyboard(scene, world);
+    Scene scene = new Scene(root, windowX, windowY, true);
+    Stop[] stops = new Stop[]
+    { new Stop(0, Color.RED), new Stop(1, Color.ORANGE) };
+    LinearGradient lg = new LinearGradient(0, 1, 0, 0, true, CycleMethod.NO_CYCLE, stops);
+    scene.setFill(lg);
+    scene.setCamera(camera);
 
-		primaryStage.setTitle("Application");
-		primaryStage.setScene(scene);
-		primaryStage.show();
+    handleMouse(scene, world);
+    handleKeyboard(scene, world);
 
-		AnimationTimer gameLoop = new MainGameLoop();
-		gameLoop.start();
+    primaryStage.setTitle("Application");
+    primaryStage.setScene(scene);
+    primaryStage.show();
 
-	}
+    AnimationTimer gameLoop = new MainGameLoop();
+    gameLoop.start();
 
-	class MainGameLoop extends AnimationTimer {
+  }
 
-		public void handle(long now) {
-			double cos = Math.cos(Math.toRadians(cameraXform.ry.getAngle()));
-			double sin = Math.sin(Math.toRadians(cameraXform.ry.getAngle()));
+  class MainGameLoop extends AnimationTimer
+  {
 
-			double z = mapXform.t.getTz();
-			double x = mapXform.t.getTx();
+    public void handle(long now)
+    {
+      double cos = Math.cos(Math.toRadians(cameraXform.ry.getAngle()));
+      double sin = Math.sin(Math.toRadians(cameraXform.ry.getAngle()));
 
-			/* Moves the world in realtion to the camera */
-			if (front) {
-				mapXform.t.setX(x - (speed * sin));
-				mapXform.t.setZ(z - (speed * cos));
+      double z = mapXform.t.getTz();
+      double x = mapXform.t.getTx();
 
-			}
-			if (back) {
-				mapXform.t.setX(x + (speed * sin));
-				mapXform.t.setZ(z + (speed * cos));
+      /* Moves the world in realtion to the camera */
+      if (front)
+      {
+        mapXform.t.setX(x - (speed * sin));
+        mapXform.t.setZ(z - (speed * cos));
 
-			}
-			if (left) {
-				mapXform.t.setX(x - (speed * cos));
-				mapXform.t.setZ(z + (speed * sin));
+      }
+      if (back)
+      {
+        mapXform.t.setX(x + (speed * sin));
+        mapXform.t.setZ(z + (speed * cos));
 
-			}
-			if (right) {
-				mapXform.t.setX(x + (speed * cos));
-				mapXform.t.setZ(z - (speed * sin));
+      }
+      if (left)
+      {
+        mapXform.t.setX(x - (speed * cos));
+        mapXform.t.setZ(z + (speed * sin));
 
-			}
+      }
+      if (right)
+      {
+        mapXform.t.setX(x + (speed * cos));
+        mapXform.t.setZ(z - (speed * sin));
 
-		}
-	}
+      }
 
-	public static void main(String[] args) {
-		launch(args);
-	}
+    }
+  }
+
+  public static void main(String[] args)
+  {
+    launch(args);
+  }
 
 }
