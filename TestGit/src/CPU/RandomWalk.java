1d1a399f92513cb38e02b77fc9127f9d468e5cb0
Jesus Lopez <jlopez92@unm.edu>
2016-02-27 14:54:05 -0700

    Creates RandomWalk determineNextMove method
    Currently, a Zombie of type RandomWalk determines 1 of 4 directions
    (north, south, east, west) and move that direction. Doesn't detect for
    collisions or anything of the sort.

--- TestGit/src/CPU/RandomWalk.java
+++ TestGit/src/CPU/RandomWalk.java
@@ -1,20 +1,52 @@
 package CPU;
 
+import java.util.Random;
+
 import javafx.scene.shape.Cylinder;
 
 public class RandomWalk extends Zombie
 {
+  private static Random rand;
+  private double translation;
 
   public RandomWalk(int x, int y, Cylinder m)
   {
     super(x, y, m);
+    rand = new Random();
   }
 
   @Override
   public void determineNextMove()
-  {
-    // TODO Auto-generated method stub
-    
+  {    
+	  int z = rand.nextInt(4) + 1;
+	  
+	  switch (z) //number 1 through 4
+	  {
+	    case 1:
+	    {
+	    	translation = super.model.getTranslateX() + 1;
+	    	super.model.setTranslateX(translation);
+	    	break;
+	    }
+	    case 2:
+	    {
+	    	translation = super.model.getTranslateZ() + 1;
+	    	super.model.setTranslateZ(translation);	    	
+	    	break;
+	    }
+	    case 3:
+	    {
+	    	translation = super.model.getTranslateX() - 1;
+	    	super.model.setTranslateX(translation);
+	    	break;
+	    }
+	    default:
+	    {
+	    	translation = super.model.getTranslateZ() - 1;
+	    	super.model.setTranslateZ(translation);	  
+	    	break;
+	    }
+	  }
   }
 
 }
