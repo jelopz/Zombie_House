13c87d1d4d4223f020bf420213f94f894a9da98b
Jesus Lopez <jlopez92@unm.edu>
2016-02-27 18:37:45 -0700

    Adds a static random variable in Zombie abstract class.

--- TestGit/src/CPU/RandomWalk.java
+++ TestGit/src/CPU/RandomWalk.java
@@ -6,13 +6,11 @@ import javafx.scene.shape.Cylinder;
 
 public class RandomWalk extends Zombie
 {
-  private static Random rand;
   private double translation;
 
   public RandomWalk(int x, int y, Cylinder m)
   {
     super(x, y, m);
-    rand = new Random();
   }
 
   @Override
@@ -24,26 +22,26 @@ public class RandomWalk extends Zombie
     {
       case 1:
       {
-        translation = super.model.getTranslateX() + 1;
-        super.model.setTranslateX(translation);
+        translation = model.getTranslateX() + 1;
+        model.setTranslateX(translation);
         break;
       }
       case 2:
       {
-        translation = super.model.getTranslateZ() + 1;
-        super.model.setTranslateZ(translation);
+        translation = model.getTranslateZ() + 1;
+        model.setTranslateZ(translation);
         break;
       }
       case 3:
       {
-        translation = super.model.getTranslateX() - 1;
-        super.model.setTranslateX(translation);
+        translation = model.getTranslateX() - 1;
+        model.setTranslateX(translation);
         break;
       }
       default:
       {
-        translation = super.model.getTranslateZ() - 1;
-        super.model.setTranslateZ(translation);
+        translation = model.getTranslateZ() - 1;
+        model.setTranslateZ(translation);
         break;
       }
     }
--- TestGit/src/CPU/Zombie.java
+++ TestGit/src/CPU/Zombie.java
@@ -1,6 +1,7 @@
 package CPU;
 
 import java.awt.Point;
+import java.util.Random;
 
 import javafx.scene.shape.Cylinder;
 
@@ -8,11 +9,13 @@ public abstract class Zombie
 {
   protected final Point spawnPoint; // (x,y) coordinate of spawn Point
   protected Cylinder model;
+  protected static Random rand;
 
   public Zombie(int x, int y, Cylinder m)
   {
     spawnPoint = new Point(x, y);
     model = m;
+    rand = new Random();
   }
 
   abstract public void determineNextMove();
