419d008b982f6aa2f94a1faf2651ffb7f645f39a
Jesus Lopez <jlopez92@unm.edu>
2016-02-29 23:45:10 -0700

    Zombies now have a hitbox variable

--- TestGit/src/CPU/Zombie.java
+++ TestGit/src/CPU/Zombie.java
@@ -2,7 +2,7 @@ package CPU;
 
 import java.awt.Point;
 import java.util.Random;
-
+import Hitbox.Hitbox;
 import javafx.scene.Group;
 
 public abstract class Zombie
@@ -10,12 +10,14 @@ public abstract class Zombie
   protected final Point spawnPoint; // (x,y) coordinate of spawn Point
   protected Group model;
   protected static Random rand;
+  protected Hitbox hitbox;
 
   public Zombie(int x, int y, Group m)
   {
     spawnPoint = new Point(x, y);
     model = m;
     rand = new Random();
+    hitbox = new Hitbox(model);
   }
 
   abstract public void determineNextMove();
